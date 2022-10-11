2a7b1d5c4e24d97f75273b041465c749ad9a91e7
Francisco Quintero Coronell <frajaquico@yahoo.es>
2022-10-11 16:18:57 -0500

    Obtain code and access_token to talk to Pocket API

--- app/controllers/oauth_controller.rb
+++ app/controllers/oauth_controller.rb
@@ -5,10 +5,18 @@ class OauthController < ApplicationController
     connect_client = Pocket::Oauth::Connect.new
     code = connect_client.connect
 
+    pt = PocketToken.new(code: code)
+    pt.save(validate: false)
+
     redirect_to connect_client.authorize(code)
   end
 
   def callback
+    pt = PocketToken.last
 
+    access_token = Pocket::Oauth::Connect.new.pocket_token(pt.code)
+    pt.update(token: access_token)
+
+    redirect_to root_path
   end
 end
